a1257ae3ef4e73aa427d17705214303f52ddf340
Once <unnfly@gmail.com>
2014-03-24 02:20:53 +0300

    implement find method in find by attributes action

--- lib/YiiAjaxFindByAttributesAction.php
+++ lib/YiiAjaxFindByAttributesAction.php
@@ -1,4 +1,52 @@
 <?php
 class YiiAjaxFindByAttributesAction extends YiiAjaxFindAction {
 
+	/**
+	 * @var array
+	 */
+	public $requestAttributesNames = array();
+
+	/**
+	 * @var callable|Closure
+	 */
+	public $requestAttributeValueValidator;
+
+	/**
+	 * You need to fill $response or $scope
+	 *
+	 * @return void
+	 */
+	protected function find() {
+
+		//  set default validator
+		if (!$this->requestAttributeValueValidator) {
+			$this->requestAttributeValueValidator = function ($attribute, $value) {
+				return !($value === '');
+			};
+		}
+
+		//  fetch attributes
+		$attributes = array();
+		foreach ($this->requestAttributesNames as $attribute) {
+			$value = Yii::app()->getRequest()->getParam($attribute);
+			if (call_user_func($this->requestAttributeValueValidator, $attribute, $value)) {
+				$attributes[$attribute] = $value;
+			}
+		}
+
+		//  find models
+		$models = array();
+		if (!empty($attributes)) {
+			$models = $this->model->findAllByAttributes($attributes);
+		}
+
+		//  set response
+		if ($this->enableViewRender) {
+			$this->scope['models'] = $models;
+		} else {
+			foreach ($models as $model) {
+				$this->response[] = $this->getAttributes($model);
+			}
+		}
+	}
 }
