72a73b2c08b318aed03bff56640587bebd630977
Once <unnfly@gmail.com>
2014-03-24 01:44:30 +0300

    implement view render action

--- lib/YiiAjaxViewRenderAction.php
+++ lib/YiiAjaxViewRenderAction.php
@@ -1,6 +1,43 @@
 <?php
 class YiiAjaxViewRenderAction extends YiiAjaxBaseAction {
 
-	
+	/**
+	 * @var string
+	 */
+	public $view;
 
+	/**
+	 * @var string
+	 */
+	public $layout;
+
+	/**
+	 * @var array
+	 */
+	public $scope = array();
+
+	public function onBeforeViewRender() {}
+	public function onAfterViewRender() {}
+
+	public function run() {
+		$this->responseType = 'text/html';
+
+		if ($this->layout) {
+			//  set layout
+			$this->getController()->layout = $this->layout;
+		}
+		if (!$this->view) {
+			//  if view not defined render current action view
+			$this->view = $this->getId();
+		}
+
+		$this->raiseEvent('onBeforeViewRender', new CEvent($this));
+
+		//  render view and set result to $response
+		$this->response = $this->getController()->renderPartial($this->view, $this->scope, true);
+
+		$this->raiseEvent('onAfterViewRender', new CEvent($this));
+
+		parent::run();
+	}
 }
